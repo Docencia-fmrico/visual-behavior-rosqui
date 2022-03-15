12a5e6f8619f623447f19dd4b0fcc2a0e60bf13c
Alvaro Alonso Alosete <a.alonsoa.2020@alumnos.urjc.es>
2022-03-15 11:57:42 +0100

    FollowBall struct
    Signed-off-by: Alvaro Alonso Alosete <a.alonsoa.2020@alumnos.urjc.es>

---
+++ include/visual_behavior/FollowBall.h
@@ -0,0 +1,46 @@
+// Copyright 2022 ROSqui
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VISUAL_BEHAVIOR_FOLLOWBALL_H
+#define VISUAL_BEHAVIOR_FOLLOWBALL_H
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include <string>
+#include <iostream>
+
+#include "geometry_msgs/Twist.h"
+#include "ros/ros.h"
+
+namespace visual_behavior
+{
+
+class FollowBall : public BT::ActionNodeBase
+{
+  public:
+    explicit FollowBall(const std::string& name);
+
+    void halt();
+
+    BT::NodeStatus tick();
+
+  protected:
+    ros::NodeHandle nh_;
+    ros::Publisher pub_vel_;
+};
+
+}  // namespace visual_behavior
+
+#endif  // VISUAL_BEHAVIOR_FOLLOWBALL_H
---
+++ src/visual_behavior/FollowBall.cpp
@@ -0,0 +1,67 @@
+// Copyright 2022 ROSqui
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "visual_behavior/FollowBall.h"
+
+namespace visual_behavior
+{
+
+FollowPerson::FollowBall(const std::string& name)
+: BT::ActionNodeBase(name, {})
+{
+  pub_vel_ = nh_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",100);
+}
+
+void
+FollowPerson::halt()
+{
+  ROS_INFO("FollowBall halt");
+}
+
+BT::NodeStatus
+FollowPerson::tick()
+{
+    ROS_INFO("FollowBall tick");
+
+    std::string person_x = getInput<std::string>("person_x").value();
+    std::string person_z = getInput<std::string>("person_z").value();
+
+    geometry_msgs::Twist cmd;
+
+    if (std::stod(person_x) < 235)
+      cmd.angular.z = -0.4;
+    else if (std::stod(person_x) > 305)
+      cmd.angular.z = 0.4;
+    else 
+      cmd.angular.z = 0;
+    
+    if (std::stod(person_z) > 1)
+      cmd.linear.x = 0.4;
+    else
+      cmd.linear.x = 0;
+   
+
+    pub_vel_.publish(cmd);
+
+
+    return BT::NodeStatus::SUCCESS;
+}
+
+}  // namespace visual_behavior
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<visual_behavior::FollowBall>("FollowBall");
+}
